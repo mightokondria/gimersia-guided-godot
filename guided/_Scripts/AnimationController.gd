a7e2a8f2684924bcc33470ce739924018dd1dd74
MIGHTOKONDRIA <72840695+mightokondria@users.noreply.github.com>
2025-11-11 14:14:42 +0700

    Add Sensei walk animation and update positions
    Introduces a new 'SenseiWalk' animation in TheStage.tscn and triggers it after the dialog sequence in AnimationController.gd. Also updates Sensei's initial position and adjusts child node positions in Sensei.tscn for better alignment.

--- guided/_Scripts/AnimationController.gd
+++ guided/_Scripts/AnimationController.gd
@@ -4,15 +4,18 @@ extends AnimationPlayer
 @onready var textbox: CanvasLayer = $"../TextBox"
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.set_cutscene_state(true)
+	
 	textbox.queue_text("Sensei", "Halo, selamat datang di latihan pertamamu.", "sensei_happy")
 	textbox.queue_text("Player", "Tugasmu mudah: capai bendera di ujung sana.", "player_happy")
 	textbox.queue_text("Sensei", "Hati-hati jangan sampai jatuh ya!", "sensei_happy")
 	await textbox.finished
 	print("Dialog selesai, lanjut ke aksi berikutnya!")
 	player.set_cutscene_state(false)
+	play("SenseiWalk")
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
